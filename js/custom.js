9320b31b8e00ff9520268a4789977efe5f542679
ShubhamSatnurkar <shubhamsatnurkar061@gmail.com>
2022-03-29 17:25:05 +0530

    all section completed....

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,6 @@
+
 $(document).ready(function () {
+    // Main Hero slider Start
     $("#mySlider").slick({
         centerMode: true,
         speed: 500,
@@ -39,9 +41,79 @@ $(document).ready(function () {
             }
         }]
     });
+    // Main Hero slider end
+
+    // Featured Proudct Slider Start
+    $(".product-slider").slick({
+        rows: 2,
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+
+                }
+            },
+
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+
+        ]
+    })
+    // Featured Proudct Slider End
 
 
+    // Quick Buy Items Slider Start
+    $(".item-slider").slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
 
+                }
+            },
 
-   
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+        ]
+    })
+    // Quick Buy Items Slider End
+
 });
